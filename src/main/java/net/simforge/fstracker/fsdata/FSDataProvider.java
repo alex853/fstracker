02af91f2da460076322e050800ddc3bfc21af7a2
alex853 <alexxxey.kornev@gmail.com>
2025-12-04 21:25:41 +0200

    fsuipc removed

--- src/main/java/net/simforge/fstracker/fsdata/FSDataProvider.java
+++
@@ -1,8 +0,0 @@
-package net.simforge.fstracker.fsdata;
-
-public interface FSDataProvider {
-    /**
-     * @return next record or null if no more records have been read till the moment
-     */
-    FSDataRecord next();
-}
